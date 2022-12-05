fa38b501ce87c0527f7e85b6b385668904ab5e69
beemamolshaji <beemamolshaji98@gmail.com>
2022-12-05 14:53:19 +0530

    first commit

---
+++ src/main/java/com/nest/studentapp_backend/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.nest.studentapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/")
+    public String HomePage()
+    {
+        return "Welcome to My Website";
+    }
+    @GetMapping("/contact")
+    public String ContactPage(){
+        return "Welcome to contact page";
+
+    }
+}
